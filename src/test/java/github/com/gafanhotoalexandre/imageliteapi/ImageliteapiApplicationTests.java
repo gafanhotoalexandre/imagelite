5a4ca1938cd4a40d3d7eb639ab6037b4a029b50b
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-05-17 22:55:57 -0300

    first commit

---
+++ src/test/java/github/com/gafanhotoalexandre/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package github.com.gafanhotoalexandre.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
